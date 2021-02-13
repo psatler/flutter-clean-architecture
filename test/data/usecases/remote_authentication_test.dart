376ff58e3664a45dc393a758b530fb4d24034765
Pablo Satler <pablosatler@gmail.com>
2021-02-13 13:25:57 -0300

    ♻ refactor: move pieces of code to the right location in the project

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,43 +1,18 @@
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:flutter_clean_arch/domain/usecases/usecases.dart';
-
-// sut = System Under Tests
-// AAA = arrange, act, assert
-
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    @required this.httpClient,
-    @required this.url,
-  });
-
-  Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(
-      url: url,
-      method: 'post',
-      body: params.toJson(),
-    );
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body, // optional
-  }) async {}
-}
+import 'package:flutter_clean_arch/data/http/http.dart';
+import 'package:flutter_clean_arch/data/usecases/usecases.dart';
 
 // mocks and spies
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  // sut = System Under Tests
+  // AAA = arrange, act, assert
+
   RemoteAuthentication sut;
   HttpClientSpy httpClient;
   String url;
