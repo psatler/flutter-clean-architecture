84570fec5aedd81afc6fd9c4b02ca435fca257d3
Pablo Satler <pablosatler@gmail.com>
2021-02-14 14:24:22 -0300

    ✅ test: RemoteAuthentication returns UnexpectedError on 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,7 +2,9 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_clean_arch/domain/helpers/helpers.dart';
 import 'package:flutter_clean_arch/domain/usecases/usecases.dart';
+
 import 'package:flutter_clean_arch/data/http/http.dart';
 import 'package:flutter_clean_arch/data/usecases/usecases.dart';
 
@@ -37,5 +39,22 @@ void main() {
         body: {'email': params.email, 'password': params.password},
       ),
     );
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    );
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
