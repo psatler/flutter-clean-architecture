14a38854b8a98ba9e30c081c51b775edefefcfc2
Pablo Satler <pablosatler@gmail.com>
2021-02-14 21:18:00 -0300

    :white_check_mark: test: ensure HttpAdapter throw ServerError on 500

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -35,8 +35,10 @@ class HttpAdapter implements HttpClient {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else if (response.statusCode == 204) {
       return null;
+    } else if (response.statusCode == 400) {
+      throw HttpError.badRequest;
     }
 
-    throw HttpError.badRequest;
+    throw HttpError.serverError;
   }
 }
