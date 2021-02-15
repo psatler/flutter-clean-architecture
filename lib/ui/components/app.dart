fefb346ac7965d8aba2e24679d8abdbd80377307
Pablo Satler <pablosatler@gmail.com>
2021-02-15 12:17:59 -0300

    :sparkles: feat: create layout for login header, some buttons and set some theme properties

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -2,15 +2,56 @@
 // it will can the login page
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.light); // to keep status bar white on iOS
+
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
     return MaterialApp(
       title: 'Survey app - Clean Architecture',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        accentColor: primaryColor,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorDark,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColorLight),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+          ),
+          alignLabelWithHint: true,
+        ),
+        buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.light(primary: primaryColor),
+          buttonColor: primaryColor,
+          splashColor: primaryColorLight,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
       home: LoginPage(),
     );
   }
