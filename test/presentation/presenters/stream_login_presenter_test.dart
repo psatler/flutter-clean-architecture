e6377161335d4e06a8324b101b0049b9b5481e50
Pablo Satler <pablosatler@gmail.com>
2021-02-19 22:55:45 -0300

    :white_check_mark: test: ensure StreamLoginPresenter emits error if email is invalid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
@@ -10,13 +12,27 @@ abstract class Validation {
   });
 }
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  // if we have one controller for each Stream, we don't need a broadcast. As we
+  // are using only one StreamController, we are going to use broadcast. StreamControllers
+  // are costly, that's why we are using only one instead of 5 (one for each stream).
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'Email', value: email);
+    _state.emailError = validation.validate(field: 'Email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -37,5 +53,15 @@ void main() {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'Email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    when(validation.validate(
+            field: anyNamed('field'), value: anyNamed('value')))
+        .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
