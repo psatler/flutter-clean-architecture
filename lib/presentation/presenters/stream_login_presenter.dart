de0753bf61b9fda5f0e1d3434bc8827fbc04b656
Pablo Satler <pablosatler@gmail.com>
2021-02-19 23:17:31 -0300

    :recycle: refactor: move StreamLoginPresenter to its own file

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
+import '../protocols/protocols.dart';
+
+class LoginState {
+  String emailError;
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  // if we have one controller for each Stream, we don't need a broadcast. As we
+  // are using only one StreamController, we are going to use broadcast. StreamControllers
+  // are costly, that's why we are using only one instead of 5 (one for each stream).
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  StreamLoginPresenter({@required this.validation});
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'Email', value: email);
+    _controller.add(_state);
+  }
+}
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,35 +1,9 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_clean_arch/presentation/presenters/presenters.dart';
 import 'package:flutter_clean_arch/presentation/protocols/validation.dart';
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  // if we have one controller for each Stream, we don't need a broadcast. As we
-  // are using only one StreamController, we are going to use broadcast. StreamControllers
-  // are costly, that's why we are using only one instead of 5 (one for each stream).
-  final _controller = StreamController<LoginState>.broadcast();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({@required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'Email', value: email);
-    _controller.add(_state);
-  }
-}
 
 class ValidationSpy extends Mock implements Validation {}
 
