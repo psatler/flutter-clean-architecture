2704e56c45b47ee672a026fd3323e1b610c59f40
Pablo Satler <pablosatler@gmail.com>
2021-02-21 23:34:54 -0300

    :white_check_mark: test: ensure StreamLoginPresenter emits correct events on InvalidCredentialsError

--- lib/domain/helpers/domain_errors.dart
+++ lib/domain/helpers/domain_errors.dart
@@ -2,3 +2,16 @@ enum DomainError {
   unexpected,
   invalidCredentials,
 }
+
+extension DomainErrorExtension on DomainError {
+  String get description {
+    switch (this) {
+      // this is the enum DomainError
+      case DomainError.invalidCredentials:
+        return 'Credenciais inválidas.';
+
+      default:
+        return '';
+    }
+  }
+}
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_clean_arch/domain/helpers/helpers.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -32,9 +33,14 @@ void main() {
   }
 
   PostExpectation mockAuthenticationCall() => when(authentication.auth(any));
+
   void mockAuthentication() {
     mockAuthenticationCall()
         .thenAnswer((_) async => AccountEntity(faker.guid.guid()));
+  }
+
+  void mockAuthenticationError(DomainError error) {
+    mockAuthenticationCall().thenThrow(error);
   }
 
   setUp(() {
@@ -181,6 +187,20 @@ void main() {
     sut.validatePassword(password);
 
     expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    await sut.auth();
+  });
+
+  test('Should emit correct events on InvalidCredentialsError', () async {
+    mockAuthenticationError(DomainError.invalidCredentials);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emits(false));
+    sut.mainErrorStream.listen(expectAsync1((error) => expect(
+          error,
+          DomainError.invalidCredentials.description,
+        )));
 
     await sut.auth();
   });
