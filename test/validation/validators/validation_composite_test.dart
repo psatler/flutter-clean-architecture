02ae2d858e3efb1a3da522489789a5e952d54db6
Pablo Satler <pablosatler@gmail.com>
2021-02-24 21:33:38 -0300

    :white_check_mark: test: ensure ValidationComposite returns the first error found

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -15,7 +15,14 @@ class ValidationComposite implements Validation {
 
   @override
   String validate({String field, String value}) {
-    return null;
+    String error;
+    for (final validation in validations) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error;
   }
 }
 
@@ -61,5 +68,15 @@ void main() {
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
+  });
+
+  test('Should return first error found', () {
+    mockValidation1('error_1');
+    mockValidation2('error_2');
+    mockValidation3('error_3');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_1');
   });
 }
