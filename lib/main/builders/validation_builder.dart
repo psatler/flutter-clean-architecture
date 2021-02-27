cb9774b97e49012a9d258a80b6ee436c5024f2a1
Pablo Satler <pablosatler@gmail.com>
2021-02-26 22:02:18 -0300

    :sparkles: feat: create field validations using the design pattern builder

---
+++ lib/main/builders/validation_builder.dart
@@ -0,0 +1,32 @@
+import '../../validation/protocols/protocols.dart';
+import '../../validation/validators/validators.dart';
+
+class ValidationBuilder {
+  static ValidationBuilder _instance;
+
+  // letting the constructor as private in dart
+  ValidationBuilder._();
+
+  String fieldName;
+  List<FieldValidation> validations = [];
+
+  // the only access to the builder is this static method which returns an instance of the class
+  // so then we can chain the methods
+  static ValidationBuilder field(String fieldName) {
+    _instance = ValidationBuilder._();
+    _instance.fieldName = fieldName;
+    return _instance;
+  }
+
+  ValidationBuilder required() {
+    validations.add(RequiredFieldValidation(fieldName));
+    return this; // so that we can chain the methods
+  }
+
+  ValidationBuilder email() {
+    validations.add(EmailValidation(fieldName));
+    return this;
+  }
+
+  List<FieldValidation> build() => validations;
+}
