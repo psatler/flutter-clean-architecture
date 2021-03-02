5325e1cede0bc6af0ec679a1f6e37370c58bf6c3
Pablo Satler <pablosatler@gmail.com>
2021-03-01 22:24:51 -0300

    :white_check_mark: test: ensure LocalStorageAdapter throws if save secure of FlutterSecureStorage throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -38,9 +38,23 @@ void main() {
     value = faker.guid.guid();
   });
 
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
   test('Should call save secure with correct parameters', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+// we won't treat the exception here at the infra layer, letting it to be handled at the data layer
+  test('Should throw if secure throws', () async {
+    mockSaveSecureError();
+
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(isA<Exception>()));
   });
 }
