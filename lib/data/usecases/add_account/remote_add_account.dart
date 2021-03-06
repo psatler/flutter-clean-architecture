d1b161df77f16c35884b4c3d08bfe6824bbbed20
Pablo Satler <pablosatler@gmail.com>
2021-03-06 10:29:30 -0300

    :white_check_mark: test: ensure RemoteAddAccount calls HttpClient with correct values

---
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -0,0 +1,60 @@
+import 'package:meta/meta.dart';
+
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../../domain/entities/entities.dart';
+
+import '../../http/http.dart';
+import '../../models/models.dart';
+
+class RemoteAddAccount {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAddAccount({
+    @required this.httpClient,
+    @required this.url,
+  });
+
+  Future<AccountEntity> add(AddAccountParams params) async {
+    final body = RemoteAddAccountParams.fromDomain(params).toJson();
+
+    final httpResponse = await httpClient.request(
+      url: url,
+      method: 'post',
+      body: body,
+    );
+  }
+}
+
+class RemoteAddAccountParams {
+  // piece of data the way the API expects to receive
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  RemoteAddAccountParams({
+    @required this.email,
+    @required this.password,
+    @required this.name,
+    @required this.passwordConfirmation,
+  });
+
+  factory RemoteAddAccountParams.fromDomain(AddAccountParams params) =>
+      RemoteAddAccountParams(
+        name: params.name,
+        email: params.email,
+        password: params.password,
+        passwordConfirmation: params.passwordConfirmation,
+      );
+
+  // let the json transformation here instead of the domain class (lib/domain/usecases/authentication.dart)
+  // so we decouple it from knowing what params each http client needs
+  Map toJson() => {
+        'name': name,
+        'email': email,
+        'password': password,
+        'passwordConfirmation': passwordConfirmation,
+      };
+}
