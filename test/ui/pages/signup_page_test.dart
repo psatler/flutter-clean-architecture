60d7bdbe06f6274c2549fca1ceb78cb80ca8557a
Pablo Satler <pablosatler@gmail.com>
2021-03-10 19:35:12 -0300

    :white_check_mark: test: ensure SignUpPage displays loading indicator before performing signUp of user

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -18,6 +18,7 @@ void main() {
   StreamController<UiError> passwordErrorController;
   StreamController<UiError> passwordConfirmationErrorController;
   StreamController<bool> isFormValidController;
+  StreamController<bool> isLoadingController;
 
   void initStreams() {
     nameErrorController = StreamController<UiError>();
@@ -25,6 +26,7 @@ void main() {
     passwordErrorController = StreamController<UiError>();
     passwordConfirmationErrorController = StreamController<UiError>();
     isFormValidController = StreamController<bool>();
+    isLoadingController = StreamController<bool>();
   }
 
   void mockStreams() {
@@ -38,6 +40,8 @@ void main() {
         .thenAnswer((_) => passwordConfirmationErrorController.stream);
     when(presenter.isFormValidStream)
         .thenAnswer((_) => isFormValidController.stream);
+    when(presenter.isLoadingStream)
+        .thenAnswer((_) => isLoadingController.stream);
   }
 
   void closeStreams() {
@@ -46,6 +50,7 @@ void main() {
     passwordErrorController.close();
     passwordConfirmationErrorController.close();
     isFormValidController.close();
+    isLoadingController.close();
   }
 
   Future<void> loadPage(WidgetTester tester) async {
@@ -275,5 +280,26 @@ void main() {
     await tester.pump();
 
     verify(presenter.signUp()).called(1); // method was called 1 time
+  });
+
+  testWidgets('Should present loading indicator', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should hide loading indicator', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+
+    isLoadingController.add(false);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
