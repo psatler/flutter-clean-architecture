e98943cf55ec1d1021b00940a7c61da14d1a969b
Pablo Satler <pablosatler@gmail.com>
2021-03-29 18:46:42 -0300

    :white_check_mark: test: ensure SurveysPage handles loading indicator correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+import '../../components/components.dart';
 import '../../helpers/helpers.dart';
+
 import 'components/components.dart';
 import 'surveys_presenter.dart';
 
@@ -20,19 +22,32 @@ class SurveysPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(R.strings.surveys),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0),
-        child: CarouselSlider(
-          options: CarouselOptions(
-            enlargeCenterPage: true,
-            aspectRatio: 1,
-          ),
-          items: [
-            SurveyItem(),
-            SurveyItem(),
-            SurveyItem(),
-          ],
-        ),
+      body: Builder(
+        builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            // a stream can be null so we explicitly compare to true
+            if (isLoading == true) {
+              showLoading(context);
+            } else {
+              hideLoading(context);
+            }
+          });
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: CarouselSlider(
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                aspectRatio: 1,
+              ),
+              items: [
+                SurveyItem(),
+                SurveyItem(),
+                SurveyItem(),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
