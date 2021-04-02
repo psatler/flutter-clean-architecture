1f69860e0a52f814db8b3664f0f0a17a56da7a1f
Pablo Satler <pablosatler@gmail.com>
2021-04-02 11:38:32 -0300

    :sparkles: feat: create getx login presenter

---
+++ lib/presentation/presenters/getx_surveys_presenter.dart
@@ -0,0 +1,44 @@
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/helpers/helpers.dart';
+import '../../domain/entities/entities.dart';
+import '../../domain/usecases/usecases.dart';
+
+import '../../ui/helpers/helpers.dart';
+import '../../ui/pages/surveys/surveys.dart';
+
+class GetxSurveysPresenter implements SurveysPresenter {
+  final LoadSurveys loadSurveys;
+
+  final _isLoading = true.obs;
+  final _surveys = Rx<List<SurveyViewModel>>();
+
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
+
+  GetxSurveysPresenter({
+    @required this.loadSurveys,
+  });
+
+  Future<void> loadData() async {
+    try {
+      _isLoading.value = true; // dispatching an event to start loading
+      List<SurveyEntity> surveys = await loadSurveys.load();
+
+      _surveys.value = surveys
+          .map((survey) => SurveyViewModel(
+                id: survey.id,
+                question: survey.question,
+                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
+                didAnswer: survey.didAnswer,
+              ))
+          .toList();
+    } on DomainError {
+      _surveys.subject.addError(UiError.unexpected.description);
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+}
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,49 +1,13 @@
-import 'package:flutter_clean_arch/domain/helpers/helpers.dart';
-import 'package:flutter_clean_arch/ui/helpers/helpers.dart';
-import 'package:get/get.dart';
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:intl/intl.dart';
 
-import 'package:flutter_clean_arch/domain/usecases/usecases.dart';
 import 'package:flutter_clean_arch/domain/entities/entities.dart';
+import 'package:flutter_clean_arch/domain/helpers/helpers.dart';
+import 'package:flutter_clean_arch/domain/usecases/usecases.dart';
+import 'package:flutter_clean_arch/presentation/presenters/getx_surveys_presenter.dart';
+import 'package:flutter_clean_arch/ui/helpers/helpers.dart';
 import 'package:flutter_clean_arch/ui/pages/pages.dart';
-
-class GetxSurveysPresenter implements SurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({
-    @required this.loadSurveys,
-  });
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true; // dispatching an event to start loading
-      List<SurveyEntity> surveys = await loadSurveys.load();
-
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UiError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
