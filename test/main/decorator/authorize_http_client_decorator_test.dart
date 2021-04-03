d456bc8bd0584a9e9ad126a41f3974e3b61d0ce0
Pablo Satler <pablosatler@gmail.com>
2021-04-03 11:54:50 -0300

    :white_check_mark: test: ensure Decorator returns same result as decoratee

--- test/main/decorator/authorize_http_client_decorator_test.dart
+++ test/main/decorator/authorize_http_client_decorator_test.dart
@@ -6,7 +6,7 @@ import 'package:test/test.dart';
 import 'package:flutter_clean_arch/data/http/http.dart';
 import 'package:flutter_clean_arch/data/cache/cache.dart';
 
-class AuthorizeHttpClientDecorator {
+class AuthorizeHttpClientDecorator implements HttpClient {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
   final HttpClient decoratee;
 
@@ -15,7 +15,7 @@ class AuthorizeHttpClientDecorator {
     @required this.decoratee,
   });
 
-  Future<void> request({
+  Future<dynamic> request({
     @required String url,
     @required String method,
     Map body, // optional
@@ -25,12 +25,14 @@ class AuthorizeHttpClientDecorator {
     final authorizedHeaders = headers ?? {}
       ..addAll({'x-access-token': token});
 
-    await decoratee.request(
+    final response = await decoratee.request(
       url: url,
       method: method,
       body: body,
       headers: authorizedHeaders,
     );
+
+    return response;
   }
 }
 
@@ -48,11 +50,24 @@ void main() {
   String method;
   Map body;
   String token;
+  String httpResponse;
 
   void mockToken() {
     token = faker.guid.guid();
     when(fetchSecureCacheStorage.fetchSecure(any))
         .thenAnswer((_) async => token);
+  }
+
+  void mockHttpResponse() {
+    httpResponse = faker.randomGenerator.string(50);
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+        headers: anyNamed('headers'),
+      ),
+    ).thenAnswer((_) async => httpResponse);
   }
 
   setUp(() {
@@ -68,6 +83,7 @@ void main() {
     body = {'any_key': 'any_value'};
 
     mockToken();
+    mockHttpResponse();
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () async {
@@ -104,5 +120,11 @@ void main() {
         'any_key': 'any_value',
       },
     )).called(1);
+  });
+
+  test('Should return same result as decoratee', () async {
+    final response = await sut.request(url: url, method: method, body: body);
+
+    expect(response, httpResponse);
   });
 }
