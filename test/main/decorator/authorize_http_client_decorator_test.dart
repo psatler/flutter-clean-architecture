ca0b0702b66722408ca2110db9fb01fcce18d1f3
Pablo Satler <pablosatler@gmail.com>
2021-04-03 11:42:53 -0300

    :white_check_mark: test: ensure Decorator adds news authorization header if header parameters of the decorator is null

--- test/main/decorator/authorize_http_client_decorator_test.dart
+++ test/main/decorator/authorize_http_client_decorator_test.dart
@@ -3,13 +3,16 @@ import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_clean_arch/data/http/http.dart';
 import 'package:flutter_clean_arch/data/cache/cache.dart';
 
 class AuthorizeHttpClientDecorator {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
+  final HttpClient decoratee;
 
   AuthorizeHttpClientDecorator({
     @required this.fetchSecureCacheStorage,
+    @required this.decoratee,
   });
 
   Future<void> request({
@@ -18,34 +21,68 @@ class AuthorizeHttpClientDecorator {
     Map body, // optional
     Map headers,
   }) async {
-    await fetchSecureCacheStorage.fetchSecure('token');
+    final String token = await fetchSecureCacheStorage.fetchSecure('token');
+    final authorizedHeaders = {'x-access-token': token};
+
+    await decoratee.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: authorizedHeaders,
+    );
   }
 }
 
 class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {}
 
+class HttpClientSpy extends Mock implements HttpClient {}
+
 void main() {
   AuthorizeHttpClientDecorator sut;
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  HttpClientSpy httpClient;
+
   String url;
   String method;
   Map body;
+  String token;
+
+  void mockToken() {
+    token = faker.guid.guid();
+    when(fetchSecureCacheStorage.fetchSecure(any))
+        .thenAnswer((_) async => token);
+  }
 
   setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    httpClient = HttpClientSpy();
     sut = AuthorizeHttpClientDecorator(
       fetchSecureCacheStorage: fetchSecureCacheStorage,
+      decoratee: httpClient, // the dependency/class being decorated
     );
 
     url = faker.internet.httpUrl();
     method = faker.randomGenerator.string(10);
     body = {'any_key': 'any_value'};
+
+    mockToken();
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () async {
     await sut.request(url: url, method: method, body: body);
 
     verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+
+  test('Should call decoratee with access token inside header', () async {
+    await sut.request(url: url, method: method, body: body);
+
+    verify(httpClient.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: {'x-access-token': token},
+    )).called(1);
   });
 }
