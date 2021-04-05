66095e5726cb4e335d6ec8dc2d9e7c5e6ce016ff
Pablo Satler <pablosatler@gmail.com>
2021-04-05 17:34:43 -0300

    :white_check_mark: test: ensure Composite call remote load

---
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -0,0 +1,31 @@
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:flutter_clean_arch/data/usecases/load_surveys/load_surveys.dart';
+import 'package:flutter_clean_arch/domain/entities/entities.dart';
+
+class RemoteLoadSurveysWithLocalFallback {
+  final RemoteLoadSurveys remote;
+
+  RemoteLoadSurveysWithLocalFallback({
+    @required this.remote,
+  });
+
+  Future<void> load() async {
+    await remote.load();
+  }
+}
+
+class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
+
+void main() {
+  test('Should call remote load', () async {
+    final remote = RemoteLoadSurveysSpy();
+    final sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+
+    await sut.load();
+
+    verify(remote.load()).called(1);
+  });
+}
