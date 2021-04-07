4e1c7993a122a4657bd5434513c4f6a1e63bde08
Pablo Satler <pablosatler@gmail.com>
2021-04-06 21:23:40 -0300

    :white_check_mark: test: ensure SurveyResultPage presents loading correctly

--- test/ui/pages/surveys_result_page_test.dart
+++ test/ui/pages/surveys_result_page_test.dart
@@ -14,9 +14,26 @@ class SurveyResultPresenterSpy extends Mock implements SurveyResultPresenter {}
 
 void main() {
   SurveyResultPresenterSpy presenter;
+  StreamController<bool> isLoadingController;
+
+  void initStreams() {
+    isLoadingController = StreamController<bool>();
+  }
+
+  void mockStreams() {
+    when(presenter.isLoadingStream)
+        .thenAnswer((_) => isLoadingController.stream);
+  }
+
+  void closeStream() {
+    isLoadingController.close();
+  }
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = SurveyResultPresenterSpy();
+
+    initStreams();
+    mockStreams();
 
     final surveysPage = GetMaterialApp(
       initialRoute: '/survey_result/any_survey_id',
@@ -29,12 +46,38 @@ void main() {
       ],
     );
 
-    mockNetworkImagesFor(() async => await tester.pumpWidget(surveysPage));
+    await mockNetworkImagesFor(
+      () async => await tester.pumpWidget(surveysPage),
+    );
   }
+
+  tearDown(() {
+    closeStream();
+  });
 
   testWidgets('Should call LoadSurveysResult on page load',
       (WidgetTester tester) async {
     await loadPage(tester);
     verify(presenter.loadData()).called(1);
+  });
+
+  testWidgets('Should handle loading indicator', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(false);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(null);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
